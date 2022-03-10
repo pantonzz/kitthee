58b8f566ebc7fd9762b0c407d8a8d70069622414
kitthee <earth_k8@hotmail.com>
2022-03-10 18:04:19 +0700

    BMI
    Finish

---
+++ IBM.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class IBM {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ป้อนน้ำหนัก=");
+        double weight = sc.nextDouble();
+        System.out.print("ป้อนส่วนสูง=");
+        double height = sc.nextDouble();
+
+        height/=100;
+        double bmi = weight / (height * height);
+
+        System.out.println("น้ำหนัก = "+weight);
+        System.out.println("ส่วนสูง="+height);
+        System.out.println("ดัชนีมวลกาย="+bmi);
+
+    }
+}
